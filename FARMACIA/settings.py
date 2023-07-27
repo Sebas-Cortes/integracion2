1380a3849b6213e69e098ef488e4e77a3d9ee94b
Sebastian cortes <seb.cortes@duocuc.cl>
2023-07-27 17:25:00 -0400

    Update settings.py

--- FARMACIA/settings.py
+++ FARMACIA/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sebastian21384@gmail.com'
-EMAIL_HOST_PASSWORD = 'uexmtmdxowavjltn'
+EMAIL_HOST_PASSWORD = ''
